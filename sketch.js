31bb4ecb93abb147cd9faed9e537be21ffbccb84
Pranav12309 <68505229+Pranav12309@users.noreply.github.com>
2020-07-21 19:03:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var fixed,moving ;
+
+function setup() {
+  createCanvas(800,400);
+ fixed = createSprite(400, 200, 50, 50);
+ fixed.shapeColor="red";
+ moving = createSprite(200, 200, 50, 50);
+moving.shapeColor="black";
+}
+
+function draw() {
+  background(255,255,255);
+  moving.x=mouseX  ;
+  moving.y=mouseY  ;
+if(  moving.x-fixed.x<moving.width/2+fixed.width/2  &&
+  fixed.x-moving.x<moving.width/2+fixed.width/2  &&
+  moving.y-fixed.y<moving.height/2+fixed.height/2 && 
+  fixed.y-moving.x<moving.height/2+fixed.height/2  )
+{
+  fixed.shapeColor="green";
+  moving.shapeColor="blue";
+}
+else{moving.shapeColor="black" ;
+ fixed.shapeColor="red";
+}
+  drawSprites();
+
+}
